127b76e3191757cc27d02574be629976208b528c
Sascha Eisner <office@einser.at>
2017-09-12 12:22:04 +0200

    Encoding &

--- adfocus.php
+++ adfocus.php
@@ -8,4 +8,4 @@
 	$coupons      = $order->get_used_coupons();
 	if( isset($coupons) ): foreach( $coupons as $coupon ): $voucher = $coupon; break; endforeach; endif;
 ?>
-<img src="https://www.profity.ch/imp/?s=PROFITY_ID&b=6&lp=1&ordervalue=<?= $order_value ?>&ordernumber=<?= $order_id ?><?php if( isset($voucher) ): ?>&vouchercode=<?= $voucher; ?><?php endif; ?>">
+<img src="https://www.profity.ch/imp/?s=PROFITY_ID&amp;b=6&amp;lp=1&amp;ordervalue=<?= $order_value ?>&amp;ordernumber=<?= $order_id ?><?php if( isset($voucher) ): ?>&amp;vouchercode=<?= $voucher; ?><?php endif; ?>">
